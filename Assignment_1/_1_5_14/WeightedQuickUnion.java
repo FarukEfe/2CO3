9435c7b1c49d88252d82041a7037048b8230b884
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-09-24 12:39:22 -0400

    Fixed remaining logic errors in the tree depth computation of WeightedQuickUnion.java

--- Assignment_1/_1_5_14/WeightedQuickUnion.java
+++ Assignment_1/_1_5_14/WeightedQuickUnion.java
@@ -4,18 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+// We're going to debug the depth calculation by making sure that we assign the depth of subtree to 
 public class WeightedQuickUnion extends UF {
+
     // We're going to keep track of depth at each id for optimizing the tree to shortest depth
     private List<Integer> depth = new ArrayList<Integer>();
-    public List<Integer> listDepth() { return this.depth; };
-    public int getDepth(int p) {
-        int count = 0;
-        while (p != this.id[p]) {
-            count++;
-            p = this.id[p];
-        }
-        return count;
-    }
 
     // Construct superclass
     public WeightedQuickUnion(int N) {
@@ -31,6 +24,13 @@ public class WeightedQuickUnion extends UF {
             p = this.id[p];
         }
         return p;
+    }
+
+    public List<Integer> listDepth() { return this.depth; };
+
+    public int getDepth(int p) {
+        int root = this.find(p);
+        return this.depth.get(root);
     }
 
     public void displayPath(int p) {
@@ -63,29 +63,38 @@ public class WeightedQuickUnion extends UF {
         // We don't need to update the depth values here, since depth @ indexes p and q are cold-updated inside 'find'
         if (pDepth > qDepth) {
             this.id[qRoot] = pRoot;
+            this.depth.set(pRoot, Math.max(pDepth, qDepth + 1));
         } else {
             this.id[pRoot] = qRoot;
+            this.depth.set(qRoot, Math.max(qDepth, pDepth + 1));
         }
     }
 
     public static void main(String[] args) {
         WeightedQuickUnion uf = new WeightedQuickUnion(20);
-        // Print statements b4
-        int p_depth = uf.getDepth(8);
-        boolean connected = uf.connected(3, 4);
-        System.out.println("Before:");
-        System.out.println(p_depth);
-        System.out.println(connected);
-        // Make some unions
+        // Tree1
         uf.union(3,5);
         uf.union(3,2);
         uf.union(6,2);
-        // Manually testing algorithm accuracy
-        uf.displayPath(3);
-        uf.displayPath(2);
+        // Tree2
+        uf.union(11,12);
+        uf.union(13,14);
+        uf.union(11,14);
+        // Combine two trees
+        uf.union(3, 13);
+        // Manually testing (depth) accuracy
         uf.displayPath(6);
-        int depth = uf.getDepth(6);
-        int root = uf.find(6);
-        System.out.println("Depth of node 6 is: " + depth + " with root " + root);
+        uf.displayPath(13);
+        uf.displayPath(12);
+        uf.displayPath(11);
+        // Display the full depth list (only the root values matter)
+        List<Integer> depth_list = uf.listDepth();
+        System.out.println(depth_list);
+        // These are all supposed to return the max depth of the connected tree (root 5 in our case)
+        // They're all going to return the same value
+        System.out.println("Depth of node #6: "+ uf.getDepth(6));
+        System.out.println("Depth of node #13: "+ uf.getDepth(13)); 
+        System.out.println("Depth of node #12: "+ uf.getDepth(12)); 
+        System.out.println("Depth of node #11: "+ uf.getDepth(11));
     }
 }
