bc8652ca45150f5656943f5319b0db49cec10288
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-09-25 16:31:38 -0400

    Simplified and documented CommonElements.java

--- Assignment_1/_1_4_12/CommonElements.java
+++ Assignment_1/_1_4_12/CommonElements.java
@@ -1,21 +1,21 @@
 package Assignment_1._1_4_12;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 // Provides solution to the problem on 1.4.12 on the Algorithms textbook.
 public class CommonElements {
 
-    public static void findCommons(List<Integer> sorted_1, List<Integer> sorted_2) {
-
-        int l1_size = sorted_1.size();
-        int l2_size = sorted_2.size();
+    // Finds common elements between
+    public static void findCommons(int[] list_1, int[] list_2) {
+        // As for the parameters, we're assuming that list_1 and list_2 will always be sorted.
+        // List lengths
+        int l1_size = list_1.length;
+        int l2_size = list_2.length;
+        // Starting indexes
         int i_1 = 0;
         int i_2 = 0;
+        // Run loop until reaching the end of either one of the lists
         while (i_1 < l1_size && i_2 < l2_size) {
-            int item_1 = sorted_1.get(i_1);
-            int item_2 = sorted_2.get(i_2);
+            int item_1 = list_1[i_1];
+            int item_2 = list_2[i_2];
             if (item_1 > item_2) {
                 i_2++;
             } else if (item_1 < item_2) {
@@ -27,10 +27,18 @@ public class CommonElements {
             }
         }
     }
+    /*
+    - We know the list is sorted, so after a match at indexes i, j for first and second lists (respectively), we
+      can deduce that the next match of values can only be in the next indexes.
+    - This is why we can implement a linear approach as a solution. In the worst case, the program runs through
+      the full length of list_1 and list_2, which gives us a linear time complexity of O(M + N), where M represents
+      list_1.length and N represents list_2.length.
+    */
+
 
     public static void main(String[] args) {
-        List<Integer> sorted_1 = new ArrayList<Integer>(Arrays.asList(1,1,2,3,5,8,8,9,11,15,18));
-        List<Integer> sorted_2 = new ArrayList<Integer>(Arrays.asList(1,3,8,8,9,12,19));
+        int sorted_1[] = {1,1,2,3,5,8,8,9,11,15,18};
+        int sorted_2[] = {1,3,8,8,9,12,19};
         findCommons(sorted_1, sorted_2);
     }
 
